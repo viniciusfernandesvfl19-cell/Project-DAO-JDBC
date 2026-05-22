a1eb58b4d2ef44ee00f1a7d81815b3477bae6b98
Vinicius Fernandes <vinicius.fernandes.vfl19@gmail.com>
2026-05-21 22:10:16 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,5 +26,11 @@ public class Program {
             System.out.println(obj);
         }
 
+        System.out.println("=== TESTING DATABASE : seller findAll ===");
+        list = sellerDao.findAll();
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
     }
 }
